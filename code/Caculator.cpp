f1f82030a4d0d086e5d93e00fe5726a75cbf537a
yinizhizhu <lijiahe19941013@sina.cn>
2017-09-29 16:31:10 +0800

    Calculator

--- code/Caculator.cpp
+++ code/Caculator.cpp
@@ -5,31 +5,13 @@
 2+(2*4)
 50/(50-20*2+1-9)*(10+12*12-12*10)
 50*(50-20*2+1-9)*(10+12*12-12*10)/(1*2*3*4*5*6)
-26*4-25/(125/25)
-13+7*2-8
-24*5-6
-(37-9)/7
-26*4
-63-7+8/4
-16*2/4+3
-91+9*100
-41*7/2
-88-12+12*2
-78*50-1440/12
-3856/16+85*16
-4000/(16+832/13)
-(326+95*25)/37
-(7236/18-228)*28
-(4275-24*75)/25
-45+15*6
-6*5/2*4
-30*3+8
-400/4+20
-10+12/3
-(80/20+80)/4
-70+(100-10*5)
-1+9*8*7*6*5/4/3/2+12*(10+10*2)/3
-100/4/(25/5)/2
+(1)+(1+2)+2*(1+3)
+100*(2+12)-20/3*2
+7*(8+2-(8/2+5*4))
+3+((12+7*9)-15)
+((5*(8+7)-5)+30*(7-6/3))*7
+8/4*2
+8/2*4
 
 #include <iostream>
 #include <cstring>
@@ -69,24 +51,19 @@ bool cmp(char top, char c) {
 }
 
 void pushOp(stack<int>& num, stack<char>& op, char c) {
-	if (op.empty()) {
-		//cout << "New: " << c << endl;//
-		op.push(c);
-		return;
-	}
-	//cout << "Top: " << op.top() << ", " << "New: " << c << endl;//
 	if (c == ')') {
 		for (; op.top() != '(';)
 			step(num, op);
 		op.pop();
 		return;
 	}
-	if (op.top() == '(') {
+	if (op.empty() || op.top() == '(') {
+		//cout << "New: " << c << endl;//
 		op.push(c);
 		return;
 	}
-	//cout << "-.0" << endl;
-	for (; cmp(op.top(), c);) {
+	//cout << "Top: " << op.top() << " >> " << "New: " << c << endl;//
+	for (; cmp(op.top(), c);) {		//For the order of calculating
 		step(num, op);
 		if (op.empty()) break;
 	}
@@ -96,7 +73,7 @@ void pushOp(stack<int>& num, stack<char>& op, char c) {
 int getNum(char* line, int& i, int len) {
 	int a;
 	sscanf(line + i, "%d", &a);
-	////cout << a << endl;//
+	//cout << a << endl;//
 	for (; i < len && line[i] >= '0' && line[i] <= '9'; i++);
 	return a;
 }
@@ -109,20 +86,16 @@ int main() {
 	for (; scanf("%s", line) != EOF;) {
 		i = 0;
 		len = strlen(line);
-		if (line[i] == '(') {
-			i++;
+		for (; line[i] == '('; i++)	//I's like to get many '(' continously
 			op.push('(');
-		}
 		num.push(getNum(line, i, len));
 		for (; i < len;) {
-			if (line[i] == ')')
-				pushOp(num, op, line[i++]);
+			for (; line[i] == ')'; i++)	//It's the same with '('
+				pushOp(num, op, line[i]);
 			if (i >= len) break;
 			pushOp(num, op, line[i++]);
-			if (line[i] == '(') {
-				i++;
+			for (; line[i] == '('; i++)	//...
 				op.push('(');
-			}
 			num.push(getNum(line, i, len));
 		}
 		for (; !op.empty();)
